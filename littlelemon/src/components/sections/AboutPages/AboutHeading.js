0350a4c3143f2590743e05141d17c7bf810effaf
Cleancapsule <mohddanial.nawawi@gmail.com>
2024-05-13 23:51:24 +0800

    news

--- littlelemon/src/components/sections/AboutPages/AboutHeading.js
+++ littlelemon/src/components/sections/AboutPages/AboutHeading.js
@@ -16,6 +16,15 @@ function AboutHeading() {
             <section className="image-page">
                 <img src={require('../../../assets/italian-man2.jpg')} alt="Little Lemon restaurant cuisine 1"></img>
             </section>
+            <iframe
+  title="Map of Restaurant Location"
+  width="600"
+  height="450"
+  frameBorder="0"
+  style={{ border: 0 }}
+  src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d7064.257789423569!2d-87.67488676588565!3d41.88953372979904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1slittle%20lemon%20Chicago!5e0!3m2!1sen!2smy!4v1715611673840!5m2!1sen!2smy"
+  allowFullScreen
+></iframe>
         </article>
   )
 }
