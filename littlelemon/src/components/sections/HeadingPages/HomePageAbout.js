5d7599cbe28132d63754d17b919eeea5a347c031
Cleancapsule <mohddanial.nawawi@gmail.com>
2024-05-27 18:48:03 +0800

    newabout

--- littlelemon/src/components/sections/HeadingPages/HomePageAbout.js
+++ littlelemon/src/components/sections/HeadingPages/HomePageAbout.js
@@ -4,25 +4,27 @@ import './HomePageAbout.css';
 function HomePageAbout() {
     return (
         <>
-        <article className="about-us">
-            <section className="about-text">
+        <article className="hm-text">
+            <section className="hmabout-text">
                 <h1>Little Lemon</h1>
                 <h2>Chicago</h2>
-                <p className="about-subtext">Little Lemon opened in 1995 by two Italian brothers, Adrian and Mario. 
+                <p className="hmabout-subtext">Little Lemon opened in 1995 by two Italian brothers, Adrian and Mario. 
                 Despite the city's diversity, the two brothers recognized the lack of Mediterranean cuisine in Chicago, 
                 and were inspired to bring the flavors of their hometown in Italy to the people of Chicago. The two brothers 
-                continue to oversee the Little Lemon restaurant, nearly thirty years later.</p>
+                continue to oversee the Little Lemon restaurant, nearly thirty years later.<br></br><br></br>Since its inception in 
+                1995 by two Italian brothers, Adrian and Mario, Little Lemon has been a cornerstone of Chicago's culinary 
+                scene. Recognizing a gap in Mediterranean cuisine offerings despite the city's rich diversity, the brothers 
+                embarked on a mission to introduce the vibrant flavors of their hometown in Italy to the palates of Chicagoans. 
+                Their vision was simple yet ambitious: to create a dining experience that transported guests to the sun-drenched 
+                shores of the Mediterranean with each bite.</p>
             </section>
 
-            <section className="images">
-                <img className="about-1" src={require('../../../assets/misc/italian-man2.jpg')} alt="italian1"></img>
-                <img className="about-2" src={require('../../../assets/misc/italian-man3.jpg')} alt="italian2"></img>
-                <img className="about-3" src={require('../../../assets/misc/italian-man4.jpg')} alt="italian3"></img>
-                <img className="about-4" src={require('../../../assets/misc/italian-man5.jpg')} alt="italian4"></img>
+            <section className="hm-image">
+                <img className="about-1" src={require('../../../assets/misc/italian-man5.jpg')} alt="italian1"></img>
             </section>
         </article>
         <div>
-        <Link className="about-action-button" to="/about">About Page</Link>
+        <Link className="hm-action-button" to="/about">About Page</Link>
         </div>
         </>
     );
--- littlelemon/src/components/sections/HeadingPages/HomePageHeading.js
+++ littlelemon/src/components/sections/HeadingPages/HomePageHeading.js
@@ -4,18 +4,18 @@ import './HomePageHeading.css';
 function Heading() {
     return (
         <header>
-            <article className="call-to-action">
-                <section className="hero-text">
+            <article className="hm-heading">
+                <section className="hm-heading-text">
                     <h1>Little Lemon</h1>
                     <h2>Chicago</h2>
-                    <p className="subsection">We are a family owned Mediterranean restaurant, located on  Maldove Street in Chicago, Illionis. We focus
+                    <p className="hm-heading-text2">We are a family owned Mediterranean restaurant, located on  Maldove Street in Chicago, Illionis. We focus
                     on traditional recipes served with a modern twist.</p>
                     <br></br>
-                    <Link className="heading-action-button" to="/reservations">Reserve a table</Link>
+                    <Link className="hm-heading-button" to="/reservations">Reserve a table</Link>
                 </section>
 
-                <section className="hero-image">
-                    <img src={require('../../../assets/misc/italian-man3.jpg')} alt="Little Lemon restaurant cuisine"></img>
+                <section className="hm-heading-image">
+                    <img src={require('../../../assets/misc/italian-man2.jpg')} alt="Little Lemon restaurant cuisine"></img>
                 </section>
             </article>
         </header>
